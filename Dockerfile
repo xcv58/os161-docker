b4f347542884e00192c79765140230a77e1ce1f2
xcv58 <chenyihonglove@gmail.com>
2016-01-13 15:40:10 -0500

    add xcv58's own devtools, including zsh, tmux

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:14.04
 MAINTAINER Yihong Chen <ychen78@buffalo.edu>
 ENV USER trinity
 ENV HOME /home/${USER}
-RUN adduser --disabled-password --gecos '' --home ${HOME} ${USER}; \
+RUN adduser --disabled-password --gecos '' --shell /usr/bin/zsh --home ${HOME} ${USER}; \
         adduser ${USER} sudo; \
         echo "${USER} ALL=NOPASSWD: ALL" >> /etc/sudoers
 USER ${USER}
@@ -11,3 +11,4 @@ RUN /bin/bash /tmp/setup.sh
 ENV HOSTNAME zion
 WORKDIR ${HOME}
 RUN echo "sudo sh -c \"echo 127.0.0.1 ${HOSTNAME} >> /etc/hosts\" 2> /dev/null" >> .bashrc
+ENTRYPOINT zsh
--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 
 # install toolchanin
 sudo apt-get -qqy install software-properties-common
-sudo add-apt-repository ppa:geoffrey-challen/os161-toolchain
+sudo add-apt-repository -y ppa:geoffrey-challen/os161-toolchain
+sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get -qq update
-sudo apt-get -qqy install os161-toolchain git gcc silversearcher-ag vim tmux
+sudo apt-get -qqy install os161-toolchain git gcc silversearcher-ag vim tmux zsh curl wget make tree
+
+sudo locale-gen en_US.UTF-8
 
 # fetch os161 source code
 cd ~
@@ -24,3 +27,23 @@ bmake depend
 bmake
 bmake install
 cp /usr/share/examples/sys161/sys161.conf.sample ~/os161/root/sys161.conf
+
+# setup dev tools
+DEV_TOOLS=~/.devtools
+mkdir -p ${DEV_TOOLS}
+
+# setup zsh
+cd ${DEV_TOOLS}
+zsh
+git clone --recursive https://github.com/xcv58/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+zsh "${ZDOTDIR:-$HOME}/.zprezto/link-dot-files.zsh"
+sudo chsh -s /bin/zsh
+
+# setup tmux
+cd ${DEV_TOOLS}
+git clone https://github.com/xcv58/tmux_conf.git tmux_conf
+cd tmux_conf
+sudo zsh install.zsh
+
+# setup gdbinit
+curl -L http://os161.xcv58.me | sudo bash

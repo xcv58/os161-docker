0c727fd4dde895fa1fecd50867ce30888cae0336
xcv58 <chenyihonglove@gmail.com>
2016-02-05 14:21:11 -0500

    update expect.sh

--- expect.sh
+++ expect.sh
@@ -7,13 +7,13 @@ proc do_exit {msg} {
 set timeout 5
 spawn docker run -ti xcv58/os161
 send "bash\r"
-expect "trinity@zion:"
+expect "xcv58@"
 send "cd os161/root; sys161 kernel\r"
 expect "OS/161 kernel"
 send "?\r"
 expect "Next kernel heap generation"
 send "q\r"
 expect {
-    "trinity@zion:" { exit 0 }
+    "xcv58@" { exit 0 }
     timeout {do_exit "timed out waiting for prompt"}
 }

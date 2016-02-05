2d5ef6d6b466d713a0d795471ca4e9972a4a43e5
xcv58 <chenyihonglove@gmail.com>
2016-02-05 14:03:37 -0500

    rm docker hostname, based on xcv58/ubuntu image.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
-FROM ubuntu:14.04
-MAINTAINER Yihong Chen <ychen78@buffalo.edu>
-ENV USER trinity
+FROM xcv58/ubuntu
+MAINTAINER xcv58 <i@xcv58.com>
+ENV USER xcv58
 ENV HOME /home/${USER}
-RUN adduser --disabled-password --gecos '' --shell /usr/bin/zsh --home ${HOME} ${USER}; \
-        adduser ${USER} sudo; \
-        echo "${USER} ALL=NOPASSWD: ALL" >> /etc/sudoers
 USER ${USER}
 COPY setup.sh /tmp/setup.sh
 RUN /bin/bash /tmp/setup.sh
-ENV HOSTNAME zion
 WORKDIR ${HOME}
-RUN echo "sudo sh -c \"echo 127.0.0.1 ${HOSTNAME} >> /etc/hosts\" 2> /dev/null" >> .bashrc
 ENTRYPOINT zsh
--- setup.sh
+++ setup.sh
@@ -1,30 +1,11 @@
 #!/usr/bin/env bash
 
 # install toolchanin
-sudo apt-get -qqy install software-properties-common
 sudo add-apt-repository -y ppa:geoffrey-challen/os161-toolchain
-sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get -qq update
-sudo apt-get -qqy install os161-toolchain git gcc silversearcher-ag vim tmux zsh curl wget make tree golang
+sudo apt-get -qqy install os161-toolchain
 
 sudo locale-gen en_US.UTF-8
-
-# setup dev tools
-DEV_TOOLS=~/.devtools
-mkdir -p ${DEV_TOOLS}
-
-# setup zsh
-cd ${DEV_TOOLS}
-zsh
-git clone --recursive https://github.com/xcv58/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-zsh "${ZDOTDIR:-$HOME}/.zprezto/link-dot-files.zsh"
-sudo chsh -s /bin/zsh
-
-# setup tmux
-cd ${DEV_TOOLS}
-git clone https://github.com/xcv58/tmux_conf.git tmux_conf
-cd tmux_conf
-sudo zsh install.zsh
 
 # setup gdbinit
 curl -L http://os161.xcv58.com | sudo bash
